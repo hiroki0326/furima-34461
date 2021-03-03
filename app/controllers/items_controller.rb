f367565b767fccbfacad9bb7e4968fb6708c272d
hiroki0326 <hiroki03266@gmail.com>
2021-03-03 21:19:06 +0900

    編集画面に遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,19 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:image, :item_name, :description, :category_id, :delivery_fee_id, :status_id, :prefecture_id, :day_id, :price).merge(user_id: current_user.id)
